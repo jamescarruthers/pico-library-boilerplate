10dcda306c3d9a805bfc100e9cb8e686303f86ca
James Carruthers <james@mintylamb.co.uk>
2024-08-31 18:30:28 +0100

    Delete build directory

--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/Users/james/Library/CloudStorage/Dropbox/Apps/pico/projects/boilerplate/build/_deps/picotool/"
-};
--- build/lib/your_library/CMakeFiles/your_library.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libyour_library.a"
-)
